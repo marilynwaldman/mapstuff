abe3965806de02f4d0369f59277a173ca3bafc9c
MarilynWaldman <mdwaldman22@gmail.com>
2021-11-24 09:10:51 -0700

    see if we can get requests working on streamlit

--- apps/waterdistrict.py
+++ apps/waterdistrict.py
@@ -1,21 +1,70 @@
+from geopandas.geodataframe import GeoDataFrame
+import pandas as pd
+import numpy as np
+import geopandas as gpd
 import streamlit as st
-from streamlit_folium import folium_static
-import folium
+import folium as fl
+import streamlit_folium as sf
+import branca.colormap as cm
+import os as os
+import pathlib
+import zipfile
+import requests
+
+STREAMLIT_STATIC_PATH = pathlib.Path(st.__path__[0]) / "static"
+print(STREAMLIT_STATIC_PATH)
+# We create a downloads directory within the streamlit static asset directory
+# and we write output files to it
+DOWNLOADS_PATH = STREAMLIT_STATIC_PATH / "downloads"
+if not DOWNLOADS_PATH.is_dir():
+    DOWNLOADS_PATH.mkdir()  
+
+def download_file_from_google_drive(id, destination):
+    URL = "https://docs.google.com/uc?export=download"
+
+    session = requests.Session()
+
+    response = session.get(URL, params = { 'id' : id }, stream = True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = { 'id' : id, 'confirm' : token }
+        response = session.get(URL, params = params, stream = True)
+
+    save_response_content(response, destination)    
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+
+    return None
+
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
 
 
-with st.echo():
-    import streamlit as st
-    from streamlit_folium import folium_static
-    import folium
 
-    # center on Liberty Bell
-    m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
+def get_districtsgdf():
 
-    # add marker for Liberty Bell
-    tooltip = "Liberty Bell"
-    folium.Marker(
-        [39.949610, -75.150282], popup="Liberty Bell", tooltip=tooltip
-    ).add_to(m)
+    out_zip = os.path.join(DOWNLOADS_PATH, "Water_Districts.zip")
+    file_id = '1Ng9v8HQrTRd8BQUgIZFgbG2Ht0tCfcM2'
+    download_file_from_google_drive(file_id, out_zip)
+    zip_ref = zipfile.ZipFile(out_zip, "r")
+    zip_ref.extractall(DOWNLOADS_PATH)
 
-    # call to render Folium map in Streamlit
-    folium_static(m)
+    gdf = gpd.read_file(out_zip.replace("zip", "shp"))
+
+    return gdf
+
+
+def main():
+    waterdistrict_gdf = get_districtsgdf()
+    st.write(waterdistrict_gdf.head())
+if __name__ == "__main__":
+    main()
